8d5928874599eff8264909f3f584069a639b95c4
Sam Kramer <skramer@esri.com>
2021-01-31 00:14:56 -0500

    Added examples from Chapter 5.7

---
+++ ch05/get_min.py
@@ -0,0 +1,22 @@
+import string
+# 5.7 Dictionaries
+# Finger exercise: Implement a function that meets the specification
+
+def get_min(d):
+    """
+    d is a dict mapping letters to ints
+    Returns the value in d with the key that occurs first in the alphabet.
+    Example:
+        d = {x = 11, b = 12}
+        get_min(d) returns 12
+    """
+    letters = string.ascii_lowercase
+    for c in letters:
+        if c in d.keys():
+            return d[c]
+        
+if __name__ == "__main__":
+    d = {'x':11, 'b':12}
+    print(f"d = {d}")
+    min_val = get_min(d)
+    print(f"min_val = {min_val}")
---
+++ ch05/translate.py
@@ -0,0 +1,42 @@
+import string
+# 5.7 Dictionaries
+# Figure 5-9: Translating text (badly)
+
+EtoF = {'bread':'pain', 'wine':'vin', 'with':'avec', 'I':'Je',
+    'eat':'mange', 'drink':'bois', 'John':'Jean',
+    'friends':'amis', 'and':'et', 'of':'du', 'red':'rouge'}
+FtoE = {'pain': 'bread', 'vin': 'wine', 'avec': 'with', 'Je': 'I',
+    'mange': 'eat', 'bois': 'drink', 'Jean': 'John',
+    'amis': 'friends', 'et': 'and', 'du': 'of', 'rouge': 'red'}
+dicts = {'English to French':EtoF, 'French to English':FtoE}
+
+def translate_word(word, dictionary):
+    if word in dictionary:
+        return dictionary[word]
+    return '"' + word + '"'
+
+def translate(phrase, lang_dict):
+    UC_letters = string.ascii_uppercase
+    LC_letters = string.ascii_lowercase
+    letters = UC_letters + LC_letters
+    punctuation = string.punctuation
+    
+    translation = ''
+    word = ''
+    for c in phrase:
+        if c in letters:
+            word = word + c
+        elif word != '':
+            if c in punctuation:
+                c = c + ' '
+            translation = (translation +
+                           translate_word(word, lang_dict) + c)
+            word = ''
+    return translation
+
+if __name__ == "__main__":
+    print(translate('I drink good red wine, and eat bread.',
+                    dicts['English to French']))
+    print(translate('Je bois du vin rouge.',
+                    dicts['French to English']))
+    
